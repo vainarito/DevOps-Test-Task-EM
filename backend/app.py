bc5cb837b60ce1364c7a2d04f463f9bb1ce11c4d
Victor Prilovsky <viktor.prilovsky@gmail.com>
2026-06-29 07:43:41 +0300

    feat: add basic HTTP server

---
+++ backend/app.py
@@ -0,0 +1,20 @@
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
+class Handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == '/':
+            self.send_response(200)
+            self.send_header('Content-type', 'text/plain')
+            self.end_headers()
+            self.wfile.write(b"Hello from Effective Mobile!")
+        else:
+            self.send_response(404)
+            self.end_headers()
+
+def run(server_class=HTTPServer, handler_class=Handler, port=8080):
+    server_address = ('', port)
+    httpd = server_class(server_address, handler_class)
+    httpd.serve_forever()
+
+if __name__ == "__main__":
+    run()
